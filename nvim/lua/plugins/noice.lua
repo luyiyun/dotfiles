28a5923dc72ca66f09812a7f94d845fbd5c5a91e
Lu Yiyun <18845728185@163.com>
2023-06-14 21:14:16 +0800

    feat: rewrite config of noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -5,7 +5,7 @@ return {
   -- ---------------------
   {
     "folke/noice.nvim",
-    -- config = function() require("plugin-configs.noice") end,
+    event = "VeryLazy",
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
@@ -13,12 +13,28 @@ return {
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
-      {'nvim-telescope/telescope.nvim', tag = '0.1.0'},
     },
     cmd = "Noice",
     keys = {
       {"<leader>fn", "<cmd>Noice telescope<cr>", desc = "List noice"}
     },
-    config = function () require("noice").setup() end
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    }
   },
 }
