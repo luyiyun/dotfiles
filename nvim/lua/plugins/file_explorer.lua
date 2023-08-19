0f1e0f5ef4a26ff951d5875fb7c9a0ec8ebdda4e
Luyiyun <18845728185@163.com>
2023-08-19 16:24:02 +0800

    feat: add file explorer (neo-tree)

---
+++ nvim/lua/plugins/file_explorer.lua
@@ -0,0 +1,95 @@
+return {
+  -- {
+  --   'nvim-tree/nvim-tree.lua',
+  --   dependencies = 'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  --   keys = {
+  --     -- {"<leader>tt", function ()
+  --     --   require("nvim-tree.api").tree.toggle({path = get_root()})
+  --     -- end, desc = "toggle file explorer[nvim-tree]"},
+  --     -- NOTE:使用下面的sync_root_with_cwd配置也可以达到目的
+  --     {"<leader>tt", "<cmd>NvimTreeToggle<CR>", desc = "toggle file explorer[nvim-tree]"},
+  --     {"<leader>tf", "<cmd>NvimTreeFocus<CR>", desc = "focus in explorer[nvim-tree]"},
+  --     -- {"<leader>tr", "<cmd>NvimTreeRefresh<CR>", desc = "refresh file explorer[nvim-tree]"},
+  --     -- {"<leader>to", "<cmd>NvimTreeFindFileToggle<CR>", desc = "toggle file finder explorer[nvim-tree]"}
+  --   },
+  --   config = function()
+  --     vim.g.loaded_netrw       = 1
+  --     vim.g.loaded_netrwPlugin = 1
+  --     require("nvim-tree").setup({
+  --       disable_netrw = true,
+  --       hijack_netrw = true,
+  --       sync_root_with_cwd = true,  -- 自动根据当前的文件更新root
+  --       update_focused_file = {
+  --         enable = true,  -- 自动打开文件所在的路径
+  --       }
+  --     })
+  --     -- require("nvim-tree.events").on_file_created(function(file) vim.cmd("edit " .. file.fname) end)
+  --   end,
+  -- },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+    keys = {
+      {
+        "<leader>tt",
+        function() require("neo-tree.command").execute({ toggle = true, dir = get_root() }) end,
+        desc = "Explorer NeoTree (root dir)",
+      },
+      {
+        "<leader>te",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+    },
+    deactivate = function() vim.cmd([[Neotree close]]) end,
+    init = function()
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("neo-tree")
+        end
+      end
+    end,
+    opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("neo-tree").setup(opts)
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "*lazygit",
+        callback = function()
+          if package.loaded["neo-tree.sources.git_status"] then
+            require("neo-tree.sources.git_status").refresh()
+          end
+        end,
+      })
+    end,
+  }
+}
