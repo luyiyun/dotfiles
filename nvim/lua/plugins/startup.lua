5317b41cc47ef7c16dabb6f4d01966df04c1c720
Luyiyun <18845728185@163.com>
2023-08-19 17:05:24 +0800

    feat: fix event of startup as VimEnter, not VeryLazy

--- nvim/lua/plugins/startup.lua
+++ nvim/lua/plugins/startup.lua
@@ -47,7 +47,7 @@ return {
   -- },
   {
     'goolord/alpha-nvim',
-    event = "VeryLazy",
+    event = "VimEnter",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
         -- require'alpha'.setup(require'alpha.themes.startify'.config)  -- vim-startify theme
--- nvim/lua/plugins_bak/treesitter.lua
+++ nvim/lua/plugins_bak/treesitter.lua
@@ -7,26 +7,8 @@ return {
     -- version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    -- dependencies = {
-    --   {
-    --     "nvim-treesitter/nvim-treesitter-textobjects",
-    --     init = function()
-    --       -- disable rtp plugin, as we only need its queries for mini.ai
-    --       -- In case other textobject modules are enabled, we will load them
-    --       -- once nvim-treesitter is loaded
-    --       require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-    --       load_textobjects = true
-    --     end,
-    --   },
-    -- },
-    -- build = function()
-    --   local installer = loadModule("nvim-treesitter.install", "plugin-configs")
-    --   installer.prefer_git = false
-    --   installer.compilers = {"gcc", "clang"}
-    --   local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-    --   ts_update()
-    -- end,
-    -- config = function() require("plugin-configs.nvim-treesitter") end,
+    -- treesitter的一个模块，其可以为嵌套的括号显示不同的颜色
+    dependencies = "p00f/nvim-ts-rainbow",
     cmd = { "TSUpdateSync" },
     opts = {
       highlight = { enable = true },
@@ -59,14 +41,6 @@ return {
         -- termcolors     = {}
       }
     },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end
+    config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end
   },
-  -- treesitter的一个模块，其可以为嵌套的括号显示不同的颜色
-  {
-    "p00f/nvim-ts-rainbow",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-  },
-
 }
