3055354ece8104936376eb54fecd8f3b8fc14074
Luyiyun <18845728185@163.com>
2023-09-04 11:19:49 +0800

    feat: add keymap to dismiss all notifications generated by nvim-notify

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -15,7 +15,14 @@ return {
     },
     cmd = "Noice",
     keys = {
-      {"<leader>fn", "<cmd>Noice telescope<cr>", desc = "List noice"}
+      { "<leader>fn", "<cmd>Noice telescope<cr>", desc = "List noice" },
+      {
+        "<leader>nd",
+        function()
+          require("notify").dismiss({ pending = true, silent = true })
+        end,
+        desc = "dismiss all the messages of notify"
+      },
     },
     opts = {
       lsp = {
@@ -28,11 +35,11 @@ return {
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = false, -- position the cmdline and popupmenu together
+        bottom_search = false,        -- use a classic bottom cmdline for search
+        command_palette = false,      -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
       },
     }
   },
