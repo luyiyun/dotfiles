82ee3383860b76a153d0c8ac1814888e15be0e40
Rong Zhiwei <18845728185@163.com>
2023-09-08 13:20:48 +0800

    feat: add distant, but it can not work

---
+++ nvim/lua/plugins/distant.lua
@@ -0,0 +1,14 @@
+return {
+  -- {
+  --   'chipsenkbeil/distant.nvim',
+  --   branch = 'v0.3',
+  --   config = function()
+  --     require('distant'):setup({
+  --       client = {
+  --         bin = 'C:\\Softwares\\distant\\distant.exe'
+  --       }
+  --     })
+  --   end
+  -- },
+
+}
